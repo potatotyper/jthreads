7987a21e83bebceb24853c781d01a3b6620df0a3
James <jjforce17@gmail.com>
2026-05-09 15:07:20 -0700

    update port

--- visualizer/vite.config.ts
+++ visualizer/vite.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const VISUALIZER_PORT = 5173;
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: VISUALIZER_PORT,
+    port: 5173,
     strictPort: true,
   },
   preview: {
-    host: "0.0.0.0",
-    port: VISUALIZER_PORT,
+    port: 5173,
     strictPort: true,
   },
-});
+})
+
