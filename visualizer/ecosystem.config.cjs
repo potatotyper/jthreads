debdc534c283aa9cbcc9cb31db92153c47720394
James <jjforce17@gmail.com>
2026-05-09 16:04:23 -0700

    fix porting issue

--- visualizer/ecosystem.config.cjs
+++ visualizer/ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'jthreads-visualizer',
       script: './node_modules/serve/build/main.js',
-      args: '-s dist -l tcp://0.0.0.0:5173',
+      args: '-s dist -l tcp://0.0.0.0:5173 --no-port-switching',
       cwd: __dirname,
       env: {
         NODE_ENV: 'production',
